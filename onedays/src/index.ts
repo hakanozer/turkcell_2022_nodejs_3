fa2c6addd037fa8f12c7a7417568986e203517e8
Hakan ÖZER <hakanozer02@gmail.com>
2022-09-20 14:29:00 +0300

    EJS Config

--- onedays/src/index.ts
+++ onedays/src/index.ts
@@ -1,13 +1,22 @@
 import express from "express";
+import path from "path";
 const app = express()
 const port = 4000
+
+// EJS config
+app.set( "views", path.join( __dirname, "views" ) );
+app.set('view engine', 'ejs');
 
 
 // Router
 app.get('/', (req, res) => {
-    res.send('<h1>Welcome NodeJS</h1>')
+    const sendData = {
+        title: 'App Title',
+        year: 2022
+    }
+    res.render('index', { data: sendData })
 })
 
 app.listen(port, () => {
-    console.log("Listen Port:", port);
+    console.log("Listen Port:", 'http://localhost:'+ port);
 })
