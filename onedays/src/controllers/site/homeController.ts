7ae13c95f566c482e5cc4d77ddd2cd0840bcc777
Hakan ÖZER <hakanozer02@gmail.com>
2022-09-20 15:54:19 +0300

    Include - EJS Header

--- onedays/src/controllers/site/homeController.ts
+++ onedays/src/controllers/site/homeController.ts
@@ -3,9 +3,11 @@ export const homeController = express.Router()
 
 
 homeController.get('/', (req, res) => {
+    const cities = [ "İstanbul", "İzmir", "Ankara", "Samsun", "Antalya" ]
     const sendData = {
         title: 'App Title',
-        year: 2022
+        year: 2022,
+        cities
     }
     res.render('site/home', { data: sendData })
 })
